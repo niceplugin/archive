a0ac16f73ba22f6f081580b05fdc24b696e8a1cd
golden <niceplugin@gmail.com>
2021-07-17 13:01:14 +0900

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Image minify client
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+
+  // 웹팩 빌드시 어디에 어떻게 출력물을 만들지 설정
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2'
+  },
+  // output: {
+  //   filename: 'iife.js',
+  //   path: path.resolve(__dirname, 'dist'),
+  //   library: 'MY_MODULE_NAME', // 모듈을 내보낼 전역변수 이름
+  //   libraryExport: 'default'
+  // },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
+    ]
+  }
+};
