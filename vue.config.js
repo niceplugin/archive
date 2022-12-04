303e853a938005b8e3c2288cbf8f67f3a039f072
Niceplugin <niceplugin@gmail.com>
2022-12-04 19:14:22 +0900

    .

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,15 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/image-minify/'
+    : '/',
+  outputDir: 'image-minify',
   pwa: {
     name: 'Image Minify',
     themeColor: '#69A88D',
     manifestOptions: {
-      start_url: '/',
+      start_url: '/image-minify',
     }
   }
 }
