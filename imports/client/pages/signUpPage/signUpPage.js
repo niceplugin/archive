6075424e5372ebc24667a9d01d616be8cb34a5ce
HYOSEUNGAN <100184065+HYOSEUNGAN@users.noreply.github.com>
2022-12-06 22:55:47 +0900

    에이든 추가설정- 로컬아이디저장, base64 회원가입이미지 (#8)

--- imports/client/pages/signUpPage/signUpPage.js
+++ imports/client/pages/signUpPage/signUpPage.js
@@ -1,81 +1,75 @@
-import { Template } from 'meteor/templating'
+import { Template } from "meteor/templating";
 import { FlowRouter } from "meteor/ostrio:flow-router-extra";
 
-//<form>에 novalidate라고 속성을 추가하여 부트스트랩에서 제공하는 폼API를 활용하여 유효성검사를 하였습니다!
-
+//함수로 따로 빼서 사진을 base64인코딩하는 작업을 실행합니다
+function toBase64(file) {
+  const picture = new Promise((res) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => res(reader.result);
+    reader.onerror = () => res("");
+  });
+  return picture;
+}
 
 Template.signUpPage.events({
-  "submit #btn-signUp": function signIn(event){
-    event.preventDefault()
-    const target = event.target
-    const avatar = target.picture.value;
+  "submit #btn-signUp": async function signIn(event) {
+    event.preventDefault();
+    const target = event.target;
+    const file = target.picture.files[0];
     const name = target.name.value;
     const username = target.username.value;
     const email = target.email.value;
     const password = target.password.value;
 
-
-    //어드민 level:0 , 일반 level:1 profile:{avatar, name, level:1} }
-    const userInfo = {username,email,password,profile:{avatar, name, level:1} }
-    Accounts.createUser(userInfo, function(error){
-      if(error){
-        alert(error)
-      }else{
-        alert("🚀회원가입 되셨습니다!")
-        FlowRouter.go("/signin")
+    if (file) {
+      const avatar = await toBase64(file);
+      console.log(avatar);
+      if (avatar) {
+        const userInfo = {
+          username,
+          email,
+          password,
+          profile: { avatar, name, level: 1 },
+        };
+        Accounts.createUser(userInfo, function (error) {
+          if (error) {
+            alert(error);
+          } else {
+            alert("🚀회원가입 되셨습니다!");
+            FlowRouter.go("/signin");
+          }
+        });
+      } else {
+        alert("프로필을 입력해주세요!");
       }
-    })
+    } else {
+      alert("파일을 골라주세요!");
+    }
 
-    // set $'' 필드 넣기
-    //🚀picture  인코딩 필요함
-    //아이디, 아바타가 안들어감  => 로그인 안됌
+    // <form>에 novalidate라고 속성을 추가하여 부트스트랩에서 제공하는 폼API를 활용하여 유효성검사를 하였습니다!
+    const forms = document.querySelectorAll(".needs-validation");
 
-    // const reader = new FileReader();
-    // reader.onload = function() {
-    //   const result = reader.result;
-    // }
-    // console.log(result)
-    //
-    // const forms = document.querySelectorAll('.needs-validation')
-
-
-    // await Array.prototype.slice.call(forms)
-    //   .forEach(function (form) {
-    //     form.addEventListener('submit', function (event) {
-    //       if (!form.checkValidity()) {
-    //         event.preventDefault()
-    //         event.stopPropagation()
-    //       }
-    //       form.classList.add('was-validated')
-    //     }, false)
-    //   })
+    await Array.prototype.slice.call(forms).forEach(function (form) {
+      form.addEventListener(
+        "submit",
+        function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add("was-validated");
+        },
+        false
+      );
+    });
   },
-})
+});
 
-// (function () {
-//   'use strict'
-//
-//   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//   var forms = document.querySelectorAll('.needs-validation')
-//
-//   // Loop over them and prevent submission
-//   Array.prototype.slice.call(forms)
-//     .forEach(function (form) {
-//       form.addEventListener('submit', function (event) {
-//         if (!form.checkValidity()) {
-//           event.preventDefault()
-//           event.stopPropagation()
-//         }
-//
-//         form.classList.add('was-validated')
-//       }, false)
-//     })
-// })()
+Template.signUpPage.helpers({});
 
-Template.signUpPage.helpers({})
+Template.signUpPage.onCreated(function () {});
 
-Template.signUpPage.onCreated(function() {})
+Template.signUpPage.onRendered(function () {});
 
-Template.signUpPage.onRendered(function() {})
-
-Template.signUpPage.onDestroyed(function() {})
+Template.signUpPage.onDestroyed(function () {});
