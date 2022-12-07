fb99244f504f6490aa667a4e2be9b7cd118e0d5a
koyerin <99451825+ko-yerin@users.noreply.github.com>
2022-12-07 14:02:06 +0900

    Room (#13)
    * [create]:채팅창 ui
    
    * [create]:스크롤작업중

--- imports/client/pages/chatRoomPage/chatRoomPage.js
+++ imports/client/pages/chatRoomPage/chatRoomPage.js
@@ -1,11 +1,18 @@
-import { Template } from 'meteor/templating'
-import { Messages } from '/imports/collections'
-import { FlowRouter } from 'meteor/ostrio:flow-router-extra'
+import {Template} from 'meteor/templating'
+import {Messages} from '/imports/collections'
+import {FlowRouter} from 'meteor/ostrio:flow-router-extra'
 
-Template.chatRoomPage.onCreated(function(){
+Template.chatRoomPage.onCreated(function () {
   const roomId = FlowRouter.getParam("_id")
 
-  this.subscribe("chatMsg",roomId)
+  this.subscribe("chatMsg", roomId)
+
+});
+
+Template.chatRoomPage.onRendered(function () {
+  this.autorun(function() {
+    a.scroll(0,9104)
+  })
 });
 
 // Template.chatMsg.onDestroyed(function(){
@@ -28,65 +35,39 @@ Template.chatRoomPage.helpers({
 });
 
 Template.chatRoomPage.events({
-  "click .chat_button":function (e,t){
-    const user = Meteor.user();
-    const username = user.profile.name
-    const level = user.profile.level
-    // console.log("user",user)
-    // console.log("username",username)
-    // console.log("userprofile",level)
-    // console.log("date",new Date())
-
-    const text = t.find("textarea[name=text]").value
-    // console.log("Text",text)
-
-    const data = {
-      roomId: "3",
-      userId: user._id,
-      username:username,
-      userAvatar: "",
-      userLevel: level,
-      createdAt: new Date(),
-      message: text,
-    }
-    Meteor.call("messageInsert", data)
-
-    t.find("textarea[name=text]").value=""
-
+  "click .chat_button": function (e, t) {
+    chat_room(e, t)
   },
-  'keyup input': function (evt, t) {
-    console.log("Afas ")
 
-    if (evt.which === 13) {
-      const user = Meteor.user();
-      const username = user.profile.name
-      const level = user.profile.level
-      // console.log("user",user)
-      // console.log("username",username)
-      // console.log("userprofile",level)
-      // console.log("date",new Date())
-
-      const text = t.find("textarea[name=text]").value
-      // console.log("Text",text)
-
-      const data = {
-        roomId: "3",
-        userId: user._id,
-        username:username,
-        userAvatar: "",
-        userLevel: level,
-        createdAt: new Date(),
-        message: text,
-      }
-      Meteor.call("messageInsert", data)
+  'keyup textarea': function (e, t) {
+    if (e.which === 13) {
+      chat_room(e, t)
     }
   }
 })
 
-Template.chatRoomPage.helpers({})
+function chat_room(e, t) {
+  const user = Meteor.user();
+  const username = user.profile.name
+  const level = user.profile.level
+  // console.log("user",user)
+  // console.log("username",username)
+  // console.log("userprofile",level)
+  // console.log("date",new Date())
 
-Template.chatRoomPage.onCreated(function() {})
+  const text = t.find("textarea[name=text]").value
+  // console.log("Text",text)
 
-Template.chatRoomPage.onRendered(function() {})
+  const data = {
+    roomId: "3",
+    userId: user._id,
+    username: username,
+    userAvatar: "",
+    userLevel: level,
+    createdAt: new Date(),
+    message: text,
+  }
+  Meteor.call("messageInsert", data)
 
-Template.chatRoomPage.onRendered(function() {})
+  t.find("textarea[name=text]").value = ""
+}
