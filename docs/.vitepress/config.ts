8bd41c321e726f6bbb94b843c7a05daf29596d8b
Niceplugin <niceplugin@gmail.com>
2022-12-14 13:09:28 +0900

    Update (#16)
    * Docs: meteor 컬렉션 api 문서 생성중
    
    * -

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -13,8 +13,8 @@ const config = defineConfig({
   },
 
   lang: 'ko',
-  title: 'Meteor.js 한글',
-  description: '풀스택 node.js 프레임워크 Meteor.js로 비교할 수 없는 빠르게 훌륭한 앱 구축하기',
+  title: 'Blaze & Router',
+  description: 'Meteor.js의 코어 페키지 한글화',
   head,
   // serviceWorker: true,
 
@@ -51,6 +51,56 @@ const config = defineConfig({
     // },
 
     nav: [
+      // {
+      //   text: '스타일 가이드',
+      //   items: [
+      //     {
+      //       text: '스타일 가이드란?',
+      //       link: '/styles/introduction.md',
+      //     },
+      //     {
+      //       text: 'HTML 가이드',
+      //       link: '/styles/html.md',
+      //     },
+      //     {
+      //       text: 'Blaze 가이드',
+      //       link: '/styles/blaze-spacebars.md',
+      //     },
+      //     {
+      //       text: 'CSS 가이드',
+      //       link: '/styles/css.md',
+      //     },
+      //     {
+      //       text: 'JavaScript 가이드',
+      //       link: '/styles/js.md',
+      //     },
+      //   ],
+      // },
+      // {
+      //   text: '가이드',
+      //   items: [
+      //     {
+      //       text: 'Meteor',
+      //       link: '/meteor/guide/summary/introduction.md',
+      //     },
+      //     {
+      //       text: 'Blaze',
+      //       link: '/blaze/guide/summary/introduction.md',
+      //     },
+      //     // {
+      //     //   text: 'Router',
+      //     //   link: '/router/guide/summary/introduction.md',
+      //     // },
+      //   ]
+      // },
+      {
+        text: 'Blaze API',
+        link: '/blaze/api/template-declarations.md',
+      },
+      {
+        text: 'Router API',
+        link: '/router/api/core-methods.md',
+      },
       {
         text: '공식 사이트',
         items: [
@@ -69,65 +119,6 @@ const config = defineConfig({
         ],
       },
       {
-        text: '스타일 가이드',
-        items: [
-          {
-            text: '스타일 가이드란?',
-            link: '/styles/introduction.md',
-          },
-          {
-            text: 'HTML 가이드',
-            link: '/styles/html.md',
-          },
-          {
-            text: 'Blaze 가이드',
-            link: '/styles/blaze-spacebars.md',
-          },
-          {
-            text: 'CSS 가이드',
-            link: '/styles/css.md',
-          },
-          {
-            text: 'JavaScript 가이드',
-            link: '/styles/js.md',
-          },
-        ],
-      },
-      {
-        text: '가이드',
-        items: [
-          {
-            text: 'Meteor',
-            link: '/meteor/guide/summary/introduction.md',
-          },
-          {
-            text: 'Blaze',
-            link: '/blaze/guide/summary/introduction.md',
-          },
-          // {
-          //   text: 'Router',
-          //   link: '/router/guide/summary/introduction.md',
-          // },
-        ]
-      },
-      {
-        text: 'API',
-        items: [
-          {
-            text: 'Meteor',
-            link: '/meteor/api/core.md',
-          },
-          {
-            text: 'Blaze',
-            link: '/blaze/api/template-declarations.md',
-          },
-          {
-            text: 'Router',
-            link: '/router/api/core-methods.md',
-          },
-        ]
-      },
-      {
         text: '변경사항',
         items: [
           {
@@ -139,7 +130,7 @@ const config = defineConfig({
             link: 'https://www.blazejs.org/changelog',
           },
           {
-            text: 'Flow Router Extra',
+            text: 'Router',
             link: 'https://github.com/veliovgroup/flow-router/releases',
           },
         ]
@@ -147,81 +138,85 @@ const config = defineConfig({
     ],
 
     sidebar: {
-      '/meteor/guide/': [
-        {
-          text: 'Meteor 가이드',
-          items: [
-            {
-              text: '시작하기',
-              link: '/meteor/guide/summary/introduction.md',
-            },
-            {
-              text: '프로젝트 구조',
-              link: '/meteor/guide/summary/structure.md',
-            },
-          ],
-        },
-      ],
-      '/blaze/guide/': [
-        {
-          text: 'Blaze',
-          items: [
-            {
-              text: '소개',
-              link: '/blaze/guide/summary/introduction.md',
-            },
-          ],
-        },
-        {
-          text: 'Blaze 튜토리얼',
-          items: [
-            {
-              text: '1. 템플릿 시작하기',
-              link: '/blaze/guide/summary/spacebars-templates.md',
-            },
-            {
-              text: '2. 헬퍼 사용하기',
-              link: '/blaze/guide/summary/helpers.md',
-            },
-            {
-              text: '3. 이벤트 사용하기',
-              link: '/blaze/guide/summary/events.md',
-            },
-            {
-              text: '4. 템플릿에 데이터 전달하기',
-              link: '/blaze/guide/summary/blockArguments.md',
-            },
-            {
-              text: '5. 상태 관리와 전역 헬퍼',
-              link: '/blaze/guide/summary/state-globalHelpers.md',
-            },
-            {
-              text: '6. 조건문',
-              link: '/blaze/guide/summary/if-unless.md',
-            },
-            {
-              text: '7. 반복문',
-              link: '/blaze/guide/summary/each.md',
-            },
-          ],
-        },
-      ],
+      // '/meteor/guide/': [
+      //   {
+      //     text: 'Meteor 가이드',
+      //     items: [
+      //       {
+      //         text: '시작하기',
+      //         link: '/meteor/guide/summary/introduction.md',
+      //       },
+      //       {
+      //         text: '프로젝트 구조',
+      //         link: '/meteor/guide/summary/structure.md',
+      //       },
+      //     ],
+      //   },
+      // ],
+      // '/blaze/guide/': [
+      //   {
+      //     text: 'Blaze',
+      //     items: [
+      //       {
+      //         text: '소개',
+      //         link: '/blaze/guide/summary/introduction.md',
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     text: 'Blaze 튜토리얼',
+      //     items: [
+      //       {
+      //         text: '1. 템플릿 시작하기',
+      //         link: '/blaze/guide/summary/spacebars-templates.md',
+      //       },
+      //       {
+      //         text: '2. 헬퍼 사용하기',
+      //         link: '/blaze/guide/summary/helpers.md',
+      //       },
+      //       {
+      //         text: '3. 이벤트 사용하기',
+      //         link: '/blaze/guide/summary/events.md',
+      //       },
+      //       {
+      //         text: '4. 템플릿에 데이터 전달하기',
+      //         link: '/blaze/guide/summary/blockArguments.md',
+      //       },
+      //       {
+      //         text: '5. 상태 관리와 전역 헬퍼',
+      //         link: '/blaze/guide/summary/state-globalHelpers.md',
+      //       },
+      //       {
+      //         text: '6. 조건문',
+      //         link: '/blaze/guide/summary/if-unless.md',
+      //       },
+      //       {
+      //         text: '7. 반복문',
+      //         link: '/blaze/guide/summary/each.md',
+      //       },
+      //     ],
+      //   },
+      // ],
 
-      '/meteor/api/': [
-        {
-          text: 'Meteor 기본 API',
-          items: [
-            {
-              text: '코어',
-              link: '/meteor/api/core.md'
-            },
-            {
-              text: '서버 커넥션',
-              link: '/meteor/api/server-connections.md'
-            },
-          ]
-        },
-      ],
+      // '/meteor/api/': [
+      //   {
+      //     text: 'Meteor 기본 API',
+      //     items: [
+      //       {
+      //         text: '코어',
+      //         link: '/meteor/api/core.md'
+      //       },
+      //       {
+      //         text: '서버 커넥션',
+      //         link: '/meteor/api/server-connections.md'
+      //       },
+      //       {
+      //         text: '컬렉션',
+      //         link: '/meteor/api/collections.md'
+      //       },
+      //     ]
+      //   },
+      // ],
       '/blaze/api/': [
         {
           text: 'Template API',
@@ -283,7 +278,7 @@ const config = defineConfig({
     },
 
     footer: {
-      message: 'Translated by niceplugin@gmail.com',
+      message: 'Made by niceplugin@gmail.com',
       copyright: 'MIT Licensed | Copyright © 2022-present niceplugin@gmail.com'
     },
   },
