43ffe5199ff16dbf006fc91d51df34e55fc93c7a
Niceplugin <niceplugin@gmail.com>
2022-12-21 02:42:00 +0900

    플레이 가구성

--- guitar/index.js
+++ guitar/index.js
@@ -1,45 +1,78 @@
-import * as Tone from 'tone';
-import lineNoteSource from './lineNoteSource.js';
-import lineNoteFret from './lineNoteFret.js';
-import codeInfo from './codeInfo.js';
+import * as Tone from 'tone'
+import lineNoteSource from './lineNoteSource.js'
+import lineNoteFret from './lineNoteFret.js'
+import codeInfo from './codeInfo.js'
 
 class Guitar {
-  #line = [];
+  #line = []
 
   init() {
     if (this.#line.length) {
-      return;
+      return
     }
 
-    const self = this;
+    const self = this
     lineNoteSource.forEach(urls => {
-      const baseUrl = '/se/';
+      const baseUrl = '/se/'
       const line = new Tone
         .Sampler({ urls, baseUrl })
-        .toDestination();
+        .toDestination()
 
-      self.#line.push(line);
-    });
+      self.#line.push(line)
+    })
 
-    return Tone.loaded();
+    return Tone.loaded()
   }
 
-  play(code) {
-    const cloneCodeInfo = codeInfo[code].slice();
-    let i = cloneCodeInfo.length;
-    let delay = 0;
+  #getCodeArr(code) {
+    return codeInfo[code].slice()
+  }
+
+  #getVolumeArr(position = 0) {
+    switch (position) {
+      case 1:
+        return [ -2, -2, -4, -4, -6, -6 ]
+      case 0:
+        return [ -6, -4, -2, -2, -4, -6 ]
+      case -1:
+        return [ -6, -6, -4, -4, -2, -2 ]
+    }
+  }
+
+  #setAccent(accent = 0, volumeArr) {
+    accent *= 2
+    return volumeArr.map(vol => vol + accent)
+  }
+
+
+
+
+  #calcLength(arr, down) {
+    arr = arr.slice()
+      .reverse()
+    const i = arr.findIndex(flat => flat != null)
+
+    return down ? 6 : 5 - i
+  }
+
+  play_(code, down = true, accent = false) {
+    const cloneCodeInfo = codeInfo[code].slice()
+    const gap = 0.04
+    let i = this.#calcLength(cloneCodeInfo, down)
+    let delay = down ? 0 : i * gap
     while (i--) {
-      const flat = cloneCodeInfo[i];
-      const note = lineNoteFret[i][flat];
+      const flat = cloneCodeInfo[i]
+      const note = lineNoteFret[i][flat]
       if (flat == null) {
-        continue;
+        continue
       }
-      this.#line[i].triggerAttack(note, Tone.now() + delay);
-      delay += 0.015;
+      this.#line[i].volume.value = accent ? 0 : -6
+      this.#line[i].triggerAttack(note, Tone.now() + delay)
+      delay += down ? gap : -gap
     }
   }
 }
 
-const guitar = new Guitar();
+const guitar = new Guitar()
 
-export default guitar;
+export default guitar
--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-import '/style.css';
-import guitar from '/guitar';
+import '/style.css'
+import guitar from '/guitar'
 
-const btnInit = document.getElementById('btn-init');
-const btnDo = document.getElementById('btn-do');
+const btnInit = document.getElementById('btn-init')
+const btnDo = document.getElementById('btn-do')
 btnInit.addEventListener('click', async () => {
-  await guitar.init();
-  btnDo.disabled = false;
+  await guitar.init()
+  btnDo.disabled = false
   guitar.play('C')
-});
+})
+const temp = {
+
+}
