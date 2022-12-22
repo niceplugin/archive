ece7d0d1552213843fef1552fe22e7de322f61bd
Niceplugin <niceplugin@gmail.com>
2022-12-22 14:53:15 +0900

    init

---
+++ src/components/KeyCodeScreen.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card
+    class="d-flex align-center justify-center text-h2 bg-blue font-weight-bold"
+    width="160"
+    height="96"
+  >
+    {{ codeText }}
+  </v-card>
+</template>
+
+<script setup>
+import codeArray from '@/composibles/codeArray'
+import { computed } from 'vue'
+
+const codeText = computed(() => codeArray.value.join('') || '-')
+
+</script>
+
+<style scoped>
+
+</style>
